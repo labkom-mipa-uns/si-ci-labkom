8eef1d304219e21392bb32e69cd5890f46334cf9
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-10-17 13:08:51 +0700

    Update Logo Labkom

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -2,9 +2,9 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4 bg-blue">
     <!-- Brand Logo -->
     <a href="#" class="brand-link bg-blue">
-      <img src="<?php echo base_url() ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="<?php echo base_url() ?>assets/dist/img/LogoLab.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light ">LABKOM FMIPA UNS</span>
+      <span class="brand-text font-weight-bold ">LABKOM FMIPA UNS</span>
     </a>
 
     <!-- Sidebar -->
