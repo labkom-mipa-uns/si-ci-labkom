937b4715f5970463ed2eac6cf519fa3c837d6e42
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-10-23 21:21:59 +0700

    update navbar

--- application/views/User/Layouts/navbar.php
+++ application/views/User/Layouts/navbar.php
@@ -9,11 +9,10 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ml-auto">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo base_url() ?>User/C_User/services">Layanan</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Team</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="<?php echo base_url() ?>User/C_User/services">Pelayanan</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Tentang</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Kontak</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Admin</a></li>
                     </ul>
                 </div>
             </div>
