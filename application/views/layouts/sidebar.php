e6aa28e6c521b5cf01893415d995290366a13694
Hanif Fauzi Hakim <haniffauzihakim4049@gmail.com>
2020-11-02 22:23:43 +0700

    Admin Backend : Tambah link admin ke user UI di logo

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -3,7 +3,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4 bg-blue">
 
     <!-- Brand Logo -->
-    <a href="#" class="brand-link bg-blue">
+    <a href="<?php echo base_url()?>User/C_User" class="brand-link bg-blue">
       <img src="<?php echo base_url() ?>assets/dist/img/LogoLab.png" alt="Logo Labkom" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-bold ">LABKOM FMIPA UNS</span>
